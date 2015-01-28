ac6708696820e98e20547569a779e77882ac5315
Danny Banh <com.danny.banh@gmail.com>
2015-01-27 20:04:30 -0700

    Just added another output

--- Money Tycoon/src/com/money/tycoon/main/HelloWorld.java
+++ Money Tycoon/src/com/money/tycoon/main/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World, this is my project!");
+		System.out.println("I hope this goes well!");
 		
 	}
 	
