9484acfc5f7e5110d16480e3e60fe7f994ad6b9a
Danny Banh <com.danny.banh@gmail.com>
2015-01-27 19:54:17 -0700

    Just added a "Hello World" output

---
+++ Money Tycoon/src/com/money/tycoon/main/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.money.tycoon.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World, this is my project!");
+		
+	}
+	
+}
