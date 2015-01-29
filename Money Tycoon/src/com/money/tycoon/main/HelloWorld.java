07dc89e981cbfa65d94eef3fd4380d3930e3577b
Danny Banh <com.danny.banh@gmail.com>
2015-01-28 23:26:12 -0700

    Created a simple menu, only displayed a message

--- Money Tycoon/src/com/money/tycoon/main/HelloWorld.java
+++ Money Tycoon/src/com/money/tycoon/main/HelloWorld.java
@@ -1,11 +1,41 @@
 package com.money.tycoon.main;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World, this is my project!");
-		System.out.println("I hope this goes well!");
+		System.out.print("Welcome to Money Tycoon");
+		menu();
+		
+	}
+	
+	public static void menu() {
+		
+		int choice;
+		Scanner scan = new Scanner(System.in);	
+		
+		System.out.println("1. New Game");
+		System.out.println("2. Load Game");
+		System.out.print("Choice: ");
+		choice = scan.nextInt();
+		
+		switch(choice) {
+		
+		case 1:
+			System.out.println("You created a new game!");
+			break;
+		
+		case 2:
+			System.out.println("You loaded a saved game!");
+			break;
+			
+		default:
+			System.out.println("Invalid Input!");
+			break;
+		
+		}
 		
 	}
 	
